cd2f97d68207f16fb3b4d8b576b55e10ee1f3a62
cactoleproso <rodjucateixeira@gmail.com>
2017-11-27 13:02:49 -0200

    a

--- ProjetoFinal/ProjetoFinal/ConectarBD.cs
+++ ProjetoFinal/ProjetoFinal/ConectarBD.cs
@@ -3,14 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MySql
+using MySql.Data.MySqlClient;
+using MySql.Data.Common;
+using System.Threading;
 
 namespace ProjetoFinal
 {
     class ConectarBD
     {
-        SqlCommand cmd = new SqlCommand();
-        
+
+        private MySqlCommand cmd;
+
+        public ConectarBD()
+        {
+            cmd = new MySqlCommand();
+            cmd.Connection = new MySqlConnection("Server=projeto;Database=test;Uid=root;Pwd=root");
+        }
+        public bool fazerlogin(string nome , string senha)
+        {
+            bool result = false;
+            try
+            {
+                cmd.Connection.Open();
+                cmd.CommandText = "SELECT * FROM login WHERE nome= @loguser AND senha = @logsenha;";
+                cmd.Parameters.AddWithValue("@loguser", nome);
+                cmd.Parameters.AddWithValue("@logsenha", senha);
+                MySqlDataReader dados = cmd.ExecuteReader();
+                result = true;
+
+            }
+            catch (MySqlException e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                cmd.Connection.Close();
+            }
+            return result;
+        }
 
     }
 }
--- ProjetoFinal/ProjetoFinal/MainWindow.xaml.cs
+++ ProjetoFinal/ProjetoFinal/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace ProjetoFinal
 {
@@ -20,6 +21,9 @@ namespace ProjetoFinal
     /// </summary>
     public partial class MainWindow : Window
     {
+        Thread lg;
+        private bool Logado = false;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,7 +31,18 @@ namespace ProjetoFinal
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            ConectarBD conectar = new ConectarBD();
+            bool variavel = conectar.fazerlogin(UsuarioTxT.Text, SenhaTxT.Text);
 
+            if (variavel)
+            {
+                MessageBox.Show("Bem vindo!");
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("usuario/senha incorreto");
+            }
         }
     }
 }
