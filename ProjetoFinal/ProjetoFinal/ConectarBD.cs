5c58a7247655ef55e16bcd6c2ad5f522e9cf76f6
CactoLeproso <rodjucateixeira@gmail.com>
2017-11-27 11:34:47 -0200

    projeto
    a

---
+++ ProjetoFinal/ProjetoFinal/ConectarBD.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql
+
+namespace ProjetoFinal
+{
+    class ConectarBD
+    {
+        SqlCommand cmd = new SqlCommand();
+        
+
+    }
+}
